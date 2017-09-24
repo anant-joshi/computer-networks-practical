176aea3b9a9251e568d26e00150727b698f68602
Anant Joshi <anantjoshi1324@gmail.com>
2017-09-25 01:44:54 +0530

    Add skeleton for sliding window

---
+++ sliding_window/Client.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Client{
+    private int txTime;
+    private int txDelay;
+    private Window window;
+
+
+    public Client(int tx, int tp){
+        this.txTime = tx;
+        this.txDelay = tp;
+    }
+}
